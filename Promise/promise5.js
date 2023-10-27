c04d8f097cd247eaaef5dfd138604c9f3845f23a
Rahul saini <rs7965498@gmail.com>
2023-10-27 10:34:05 +0530

    callback/promise

---
+++ Promise/promise5.js
@@ -0,0 +1,40 @@
+// Example using Callback:
+
+function getFile(myCallback) {
+  let req = new XMLHttpRequest();
+  req.open('GET', "mycar.html");
+  req.onload = function() {
+    if (req.status == 200) {
+      myCallback(req.responseText);
+    } else {
+      myCallback("Error: " + req.status);
+    }
+  }
+  req.send();
+}
+
+getFile(myDisplayer);
+
+// The callback is called when the file has been loaded, and it takes a single argument that contains the content of the file.
+// This is the callback function that will be called when we have a response from our request to get the file content.
+
+
+// Example using Promise:
+
+let myPromise = new Promise(function(myResolve, myReject) {
+  let req = new XMLHttpRequest();
+  req.open('GET', "mycar.html");
+  req.onload = function() {
+    if (req.status == 200) {
+      myResolve(req.response);
+    } else {
+      myReject("File not Found");
+    }
+  };
+  req.send();
+});
+
+myPromise.then(
+  function(value) {myDisplayer(value);},
+  function(error) {myDisplayer(error);}
+);
