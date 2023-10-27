b27c387846db81ac8425412bfa780b2395cf1389
Rahul saini <rs7965498@gmail.com>
2023-10-27 20:06:00 +0530

    string methods

---
+++ String/string01.js
@@ -0,0 +1,8 @@
+// lot of things can be solved by just getting map of a string characters:
+
+const map = {};
+const string = "Hello World";
+const array = string.split("");
+// console.log(array);
+array.forEach(i => map[i] ? map[i] = map[i] + 1 : map[i] = 1);
+console.log(map)
