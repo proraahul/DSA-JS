67756558766680a7fc1228453799509e4580b166
Rahul saini <rs7965498@gmail.com>
2023-10-27 20:35:14 +0530

    max char

---
+++ String/string04.js
@@ -0,0 +1,18 @@
+const getMaxChar = (string) => {
+    const map = {};
+
+    string.split("").forEach(char => {
+        map[char] = map[char] ? map[char] + 1 : 1;
+    });
+
+    max = 1;
+    char = string[0];
+    for(let k in map){
+        if(map[k] > max) {
+            max = map[k];
+            char = k
+        }
+    }
+    return char;
+}
+console.log(getMaxChar("hello world"));
