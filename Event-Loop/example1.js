b33edfbfa06a6e67b0fc1f86a4c1aa9d363f3dcb
Rahul saini <rs7965498@gmail.com>
2023-11-03 10:03:23 +0530

    event-loop

---
+++ Event-Loop/example1.js
@@ -0,0 +1,11 @@
+console.log('start');
+
+setTimeout(() => {
+    console.log('CB setTimeout')
+}, 5000);
+
+fetch('https://api.netflix.com').then(function cbF(){
+    console.log('CB Netflix');
+});
+
+console.log('End');
