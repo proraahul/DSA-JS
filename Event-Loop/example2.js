8ac29737e7c0004120698dad55c8948a9cd5bdfc
Rahul saini <rs7965498@gmail.com>
2023-11-08 21:53:58 +0530

    learn

---
+++ Event-Loop/example2.js
@@ -0,0 +1,21 @@
+function task(message) {
+    // emulate time consuming task
+    let n = 10000000000;
+    while (n > 0){
+        n--;
+    }
+    console.log(message);
+}
+
+// console.log('Start script...');
+// task('Call an API');
+// console.log('Done!');
+
+
+console.log('Start script...');
+
+setTimeout(() => {
+    task('Download a file.');
+}, 1000);
+
+console.log('Done!');
