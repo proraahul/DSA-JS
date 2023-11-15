00593552d118028b8644f47b80cb7685fe18ce64
Rahul saini <rs7965498@gmail.com>
2023-11-15 22:14:40 +0530

    inner outer

---
+++ closures/example2.js
@@ -0,0 +1,14 @@
+function foo(outer_arg){
+// console.log('outer', outer_arg);
+
+    function inner(inner_arg){
+        return outer_arg + inner_arg
+    }
+
+    return inner;
+}
+
+var get_func_inner = foo(5);
+
+console.log(get_func_inner(4));
+console.log(get_func_inner(3));
