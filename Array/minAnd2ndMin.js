2db6f25ca3a4b0229586482e2f3baae803e42b15
Rahul saini <rs7965498@gmail.com>
2023-12-17 15:57:55 +0530

    modify

--- Array/minAnd2ndMin.js
+++ Array/minAnd2ndMin.js
@@ -7,22 +7,22 @@ function minAnd2ndMin(arr,n){
         return arr;
     
     for(let i=0; i<n; i++){
-        if(arr[i] < first){
+        if(arr[i] < first)
             first = arr[i];
-        }
+        
     }
     
     for(let i=0; i<n; i++){
-        if(arr[i]<second && arr[i]>first){
+        if(arr[i]<second && arr[i]>first)
             second = arr[i];
-        }
+        
     }
     
     return [first, second];
 }
     
     
-    let arr=[3,4,7,8,2,5,8,1];
+    let arr=[3,4,7,8,2,5,8];
     let n = arr.length;
     
     let result = minAnd2ndMin(arr,n);
