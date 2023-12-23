286e10e1363bd99cbc5612a6f34d8f301e022bee
Rahul saini <rs7965498@gmail.com>
2023-12-23 20:27:22 +0530

    sort string in alphabetical

---
+++ selcetionSort/example4.js
@@ -0,0 +1,28 @@
+// ● Write a program to sort an array of strings in alphabetical order using selection sort:
+ 
+function selectionSortStrings(arr) {
+    const n = arr.length;
+  
+    for (let i = 0; i < n - 1; i++) {
+      // Find the minimum element in the unsorted part of the array
+      let minIndex = i;
+      for (let j = i + 1; j < n; j++) {
+        if (arr[j].localeCompare(arr[minIndex]) < 0) {
+          minIndex = j;
+        }
+      }
+  
+      // Swap only if the minimum element is not in its correct position
+      if (minIndex !== i) {
+        [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+      }
+    }
+  
+    return arr;
+  }
+  
+  // Example usage:
+  const inputArray = ["banana", "apple", "orange", "grape", "cherry"];
+  const sortedArray = selectionSortStrings([...inputArray]);
+  console.log("Sorted Array of Strings:", sortedArray);
+  
