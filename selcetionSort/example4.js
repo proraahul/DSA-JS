d10fb151c9838c2d9a1304e130ce2fad327dba12
Rahul saini <rs7965498@gmail.com>
2023-12-24 22:36:01 +0530

    insertion sort

--- selcetionSort/example4.js
+++ selcetionSort/example4.js
@@ -1,19 +1,19 @@
 // ● Write a program to sort an array of strings in alphabetical order using selection sort:
  
-function selectionSortStrings(arr) {
+function selectionSortStrings(arr){
     const n = arr.length;
   
-    for (let i = 0; i < n - 1; i++) {
+    for(let i = 0; i<n - 1; i++){
       // Find the minimum element in the unsorted part of the array
       let minIndex = i;
-      for (let j = i + 1; j < n; j++) {
-        if (arr[j].localeCompare(arr[minIndex]) < 0) {
+      for(let j=i + 1; j<n; j++){
+        if(arr[j].localeCompare(arr[minIndex]) < 0){
           minIndex = j;
         }
       }
   
       // Swap only if the minimum element is not in its correct position
-      if (minIndex !== i) {
+      if(minIndex !== i){
         [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
       }
     }
