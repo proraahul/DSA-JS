efb6acd9a6cd9ebde9615e508d8d72624aafdb1e
Rahul saini <rs7965498@gmail.com>
2024-02-23 00:03:54 +0530

    js file

---
+++ javascript.js
@@ -0,0 +1,22 @@
+
+//! Hoisting: 
+
+// var x = 7;
+// const getName =()=> {
+//   console.log("getName");
+// }
+
+// // var getName2 = function() {
+// //         console.log('getName2');
+// // }
+
+// // function getName3(){
+// //     console.log('getName3');
+// // }
+
+
+// getName();
+// console.log(x);
+// console.log(getName);
+
+
