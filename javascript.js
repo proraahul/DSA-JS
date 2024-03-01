ce8ed9bbb9c2ecdbca84b2db720f9b530c43199c
Rahul saini <rs7965498@gmail.com>
2024-03-01 12:38:54 +0530

    closure example

--- javascript.js
+++ javascript.js
@@ -20,3 +20,42 @@
 // console.log(getName);
 
 
+
+
+
+// function counter(){
+//     var count = 0;
+//     return function incrementCounter(){
+//         count++;
+//         console.log(count);
+//     }
+// }
+
+// var counter1 = counter();
+// counter1();
+// console.log(counter1());
+
+// Encapsulation using closures
+
+function Counter(){
+    // console.log(count);
+    var count = 0;
+
+this.incrementCounter = function(){
+    count++;
+    console.log(count);
+}
+this.decrementCounter = function(){
+    count--;
+    console.log(count);
+}
+
+
+
+}
+
+var counter1 = new Counter();
+counter1.incrementCounter();
+counter1.incrementCounter();
+counter1.incrementCounter();
+counter1.decrementCounter();
